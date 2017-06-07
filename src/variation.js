aa6474381b3b42c49c29c4cf335a1a1c27a5a460
Joe Bumbulis <joebumbulis@gmail.com>
2017-06-07 15:25:25 -0500

    importing css to js file, stuck at making proprety none work

--- src/variation.js
+++ src/variation.js
@@ -5,6 +5,8 @@ $.ajax({
   dataType: "html",
   url: "https://www.vitamix.com/Shop/Shopping-Cart"
 }).then(function(data, status) {
+  setTimeout(openModal, 3000);
+
   //check data in cart
   //if the cart is empty then do nothing
   //otherwise launch timer for openModal
@@ -21,8 +23,6 @@ $.ajax({
 function openModal() {
   $(function() {
     var docHeight = $(document).height();
-    $("body").append("<div id='overlay'></div>");
-
     $("#overlay").on("click", function() {
       $(".modal").addClass("hidden");
       $("#overlay").addClass("hidden");
@@ -42,10 +42,32 @@ function openModal() {
   var $modal = $(
     "<div class='modal'><h4 class='close-modal-btn'>X</h4><div class='welcome'><h6 class='welcome-title'>Welcome Back!</h6><p class='welcome-message'>You left something in your cart. Check out today!</p></div><img class='image' src='https://www.vitamix.com//media/other/images/xascent-series-152x290.jpg.pagespeed.ic.LSCvQwTLVj.jpg'><div class='product-details'><h5 class='product-title'>7500</h5><h6 class='cart-qty'>Qty: 1</h6><h6 class='product-color'>Black</h6></div><h6 class='multiple-items-message'>Showing 1 of 3</h6><button class='cta-cart-button'>View Cart</button></div>"
   );
+  $(function() {
+    $(".modal").css({
+      "z-index": "1000",
+      "background-color": "#fff",
+      position: "absolute",
+      left: "20%",
+      top: "20%",
+      width: "450px",
+      height: "450px"
+      // 'border': '.1rem', 'solid' "#E1E1E1"
+    });
+    $(".hidden").css({
+      display: "none"
+    });
+    $(".close-modal-btn").css({
+      margin: ".5rem",
+      float: "right",
+      color: "#E1E1E1",
+      "font-size": "1.4rem",
+      "font-weight": "100"
+    });
+  });
+
   var closeBtn = $modal.find(".close-modal-btn");
   var ctaBtn = $modal.find(".cta-cart-button");
   $("#overlay").on("click", function() {
-    console.log("hello overlay");
     $(".modal").addClass("hidden");
     $("#overlay").remove();
   });
@@ -55,25 +77,9 @@ function openModal() {
   });
   ctaBtn.on("click", function() {
     window.location.href = "https://www.vitamix.com/Shop/Shopping-Cart";
-  })
-
+  });
+  $("body").append("<div id='overlay'></div>");
   $container.append($modal);
 }
 
-//on page load: GET request to query cart.
-//if empty do nothing
-//if NOT empty, then after 3 sec timer; open Modal.
-//
-
 setTimeout(openModal, 3000);
-
-$('.modal').css({
-      "z-index": '1000',
-      "background-color": "#fff",
-      'position': 'absolute',
-      'left': '20%',
-      'top': '20%',
-      'width': '450px',
-      'height': '450px',
-      'border': '.1rem', 'solid' '#E1E1E1'
-})
