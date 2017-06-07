e20f30e1423b1d685b8b8471d6dd862f62e91173
Joe Bumbulis <joebumbulis@gmail.com>
2017-06-07 10:00:29 -0500

    modal styling complete

--- src/variation.js
+++ src/variation.js
@@ -17,11 +17,12 @@ function variation() {
   });
   var $container = $("#container");
   var $modal = $(
-    "<div class='modal'><h4 class='close-modal-btn'>X</h4><h6 class='welcome-title'>Welcome Back!</h6><p class='welcome-message'>You left something in your cart. Check out today!</p><img><div><h5 class='product-title'>name</h5><h6 class=''>Qty:</h6><h6>Color</h6></div><button class='cta-cart-button'>View Cart</button></div>"
+    "<div class='modal'><h4 class='close-modal-btn'>X</h4><div class='welcome'><h6 class='welcome-title'>Welcome Back!</h6><p class='welcome-message'>You left something in your cart. Check out today!</p></div><img class='image' src='https://www.vitamix.com//media/other/images/xascent-series-152x290.jpg.pagespeed.ic.LSCvQwTLVj.jpg'><div class='product-details'><h5 class='product-title'>7500</h5><h6 class='cart-qty'>Qty: 1</h6><h6  class='product-color'>Black</h6></div><button class='cta-cart-button'>View Cart</button></div>"
   );
   var closeBtn = $modal.find(".close-modal-btn");
   var ctaBtn = $modal.find(".cta-cart-button");
   $("#overlay").on("click", function() {
+    console.log("hello overlay");
     $(".modal").addClass("hidden");
     $("#overlay").remove();
   });
